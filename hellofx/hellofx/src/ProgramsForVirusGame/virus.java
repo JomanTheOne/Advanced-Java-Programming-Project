d95df5f4cccda5f6e0c044b784b2df1f4b2ae7fb
JomanTheOne <63939173+JomanTheOne@users.noreply.github.com>
2020-04-23 22:10:10 -0400

    virus class commit

--- hellofx/hellofx/src/ProgramsForVirusGame/virus.java
+++ hellofx/hellofx/src/ProgramsForVirusGame/virus.java
@@ -1,10 +1,34 @@
 package ProgramsForVirusGame;
 
 public class virus {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+//l
+	private String name;
+	private Object symptom;
+	private int lethality;
+	private int infectivity;
+	
+	public virus (String name, Object symptom, int lethal, int infect) {
+		this.name = name;
+		this.symptom = symptom;
+		this.infectivity = infect;
+		this.lethality = lethal;
 	}
-
+	
+	public String getVirusName() {
+		return this.name;
+	}
+	
+	public Object getSymptomName() {
+		return this.symptom;
+	}
+	
+	public int getLethal() {
+		return this.infectivity;
+	}
+	
+	public int getInfect() {
+		return this.lethality;
+	}
+	
 }
